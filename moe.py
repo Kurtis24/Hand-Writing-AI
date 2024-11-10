d86df13a61bec9adf00601294c6cf756767a9fb0
KurtisL7 <kurtislin24@gmail.com>
2024-11-10 10:32:30 -0500

    Finished the frontend

---
+++ moe.py
@@ -0,0 +1,20 @@
+# def main():
+
+#     outfile = open("output.txt", "w") 
+#     outfile.write("No new lines\n")
+#     outfile.write("No new lines") 
+#     print("using print", file = outfile)
+#     outfile.close()          
+    
+
+# if __name__=="__main__":
+#     main()
+
+def main():
+
+    infile = open("input.txt", "r")
+    
+    line = infile.readline()
+
+if __name__=="__main__":
+    main()
