ec4aa193c8a7543006410f54da123358fea56a8b
KurtisL7 <kurtislin24@gmail.com>
2024-11-17 12:44:18 -0500

    organzie everything

---
+++ Frontend UI/js/form.js
@@ -0,0 +1,36 @@
+const form = [...document.querySelector('.form').children];
+
+form.forEach((item, i) => {
+    setTimeout(() => {
+       item.style.opacity = 1; 
+    }, i*100);
+})
+
+//form validation 
+
+const name = document.querySelector('.name') || null;
+const email = document.querySelector('.email');
+const password = document.querySelector('.password');
+const submitBtn = document.querySelector('.submit-btn');
+
+if (name == null){//maens loginpage
+
+} else{
+    
+    submitBtn.addEventListener('click', () => {
+        fetch('/register-user', {
+            method: 'post',
+            headers: new Headers({'Content-Type': 'application/json'}),
+            body: JSON.stringify({
+                name: name.value, 
+                email: email.value,
+                password: password.value
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            
+        })
+    })
+
+}
